fa2bae87461d2492b60786cb953b77e331f7cf66
emilio <emilio@mpbwalt.local>
2021-11-10 12:45:40 +0100

    minor

--- ras_app/controller/experiment.py
+++ ras_app/controller/experiment.py
@@ -12,60 +12,57 @@ import traceback
 
 isCpu=True
 sys = jvm_sys("../",isCpu)
-nstep = 1000
+nstep = 500
 stime = 0.1
 tgt=2
 S=[]
+nrep=1
+tgt_v=[]
 
-rt = []
-
-optS=None
-
-pop=np.random.randint(low=10, high=100)
 
 sys.startSys(isCpu)
-r=Client("localhost:11211")
-sys.startClient(pop)
-queue=[]
-
-tgt_v = (1)/(1+0.1*tgt)*pop
+optS=None
 
 try:
-    for i in tqdm(range(nstep)):
-        state=sys.getstate(r)[0] 
+    for rep in range(nrep):
+    
+        pop=np.random.randint(low=10, high=100)
+        r=Client("localhost:11211")
+        sys.startClient(pop)
+        queue=[]
         
-        optS=[float(state[1])/tgt]
+         
+        for i in tqdm(range(nstep)):
+            state=sys.getstate(r)[0] 
+            
+            optS=[float(state[1])/tgt]
+            
+            r.set("t1_hw",optS[0])
+            if(isCpu):
+                sys.setU(optS[0],"tier1")
+            
+            queue.append(state[0])
+            S.append(optS[0])
+            tgt_v.append((1)/(1+0.1*tgt)*pop)
+            #print(state,optS,tgt_v)
+            time.sleep(stime)
         
-        r.set("t1_hw",optS[0])
-        if(isCpu):
-            sys.setU(optS[0],"tier1")
+        sys.stopClient()
+        sys.stopSystem()
         
-        queue.append(state[0])
-        S.append(optS[0])
-        #print(state,optS,tgt_v)
-        time.sleep(stime)
+        print(e)
+        
+        plt.figure()
+        plt.plot(queue,label="queuelength")
+        plt.plot(tgt_v,color='r',linestyle='--',label="tgt")
+        #plt.axhline(y=tgt_v, color='r', linestyle='--',label="tgt")
+        plt.legend()
+        plt.savefig("rt.pdf")
+        
+        plt.figure()
+        plt.plot(S,label="cores")
+        plt.savefig("core.pdf")
     
-    sys.stopClient()
-    sys.stopSystem()
-    
-    
-    T=np.linspace(0,nstep,nstep)
-    q_avg=np.divide(np.cumsum(queue)[1:],T[1:]) 
-    
-    e=abs(q_avg[-1]-tgt_v)*100/tgt_v
-    
-    print(e)
-    
-    plt.figure()
-    plt.plot(q_avg)
-    plt.axhline(y=tgt_v, color='r', linestyle='--',label="tgt")
-    plt.legend()
-    plt.savefig("rt.pdf")
-    
-    plt.figure()
-    plt.plot(S,label="cores")
-    plt.savefig("core.pdf")
-
 except Exception as ex:
     traceback.print_exception(type(ex), ex, ex.__traceback__)
     sys.stopClient()
