5720af0b6151cb8b93c9c7f568d54ca88b248399
emilio <emilio@mpbwalt.local>
2021-11-12 18:50:49 +0100

    minor

--- ras_app/controller/experiment.py
+++ ras_app/controller/experiment.py
@@ -56,7 +56,7 @@ ctrlPeriod=0.1
 
 #monitor object
 mnt=systemMnt()
-c1 = CTControllerScaleXNode(ctrlPeriod, cores_init, 100, BCs=[0.05], DCs=[0.1])
+c1 = CTControllerScaleXNode(ctrlPeriod, cores_init, 100, BCs=[0.001], DCs=[0.2])
 c1.cores=cores_init
 c1.setSLA([tgt*0.1])
 c1.monitoring=mnt
