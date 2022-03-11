2b80158865384c51d99fe855a23ad1b52b68a0c7
emilio <emilio@mpbwalt.local>
2022-03-11 16:29:39 +0100

    fixed

--- ras_app/ras_tier1/src/main/java/app/Main.java
+++ ras_app/ras_tier1/src/main/java/app/Main.java
@@ -55,7 +55,7 @@ public class Main {
 		Tier1HTTPHandler.setTier2Host(Main.tier2Host);
 		t1Entries.put("e1", Tier1HTTPHandler.class);
 		t1Entries_stimes.put("e1", 100l);
-		final SimpleTask t1 = new SimpleTask("localhost", 3000, t1Entries, t1Entries_stimes, 100, Main.isEmu, "t1",
+		final SimpleTask t1 = new SimpleTask("localhost", 3000, t1Entries, t1Entries_stimes, 1, Main.isEmu, "t1",
 				Main.jedisHost,100l);
 		t1.setHwCore(1f);
 		return new SimpleTask[] { t1 };
