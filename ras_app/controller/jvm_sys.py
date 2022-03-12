3c7949cf8a91d1f65b091562b65bb4098e858d16
emilio <emilio@mpbwalt.local>
2022-03-12 10:02:31 +0100

    test

--- ras_app/controller/jvm_sys.py
+++ ras_app/controller/jvm_sys.py
@@ -270,7 +270,7 @@ if __name__ == "__main__":
             
             g = Client("localhost:11211")
             g.set("t1_hw","1")
-            #jvm_sys.setU(1.0,"tier1")    
+            jvm_sys.setU(1.0,"tier1")    
             mnt = Client("localhost:11211")
             
             X=[]
